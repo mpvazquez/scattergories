77e4b2eb6aa8ce722ee5f6fafe95e86568fb6326
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-03-23 21:23:40 -0400

    mounted server into Gemfile/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Scattergories::Application.routes.draw do
   root "rounds#new"
 
   get "rounds/category", to: "rounds#category"
-
   get "rounds/letter", to: "rounds#get_letter"
-
   get "rounds/auto_reject", to: "rounds#auto_reject"
 
   resources :rounds, only: [:show, :create]
+
+  mount Redis::Engine, at: "/redis"
 end
